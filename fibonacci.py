a80df16bf3603c0f21331fd50a3c7ebc5150f53d
Ashour Dankha <dankha90@gmail.com>
2017-10-06 15:55:20 -0500

    Fibonacci: Recursion v Dynamic Programming

--- fibonacci.py
+++ fibonacci.py
@@ -12,9 +12,8 @@ The following program demonstrates how to solve Fibonacci in two different ways:
 
     Essentially 1) goes over a lot of the same numbers it has already visited. That is why DP is useful here!
 
-    Note: 2) is good because you hold all fibonacci numbers up to the number the user enters.
-    If you want to find a different fibonacci number < the one user initially entered in,
-    then you have O(1) access, whereas in method 1) you have to iterate through recursively.
+    Note: I show 2 ways of using DP. One returns the entire array / sequence of fibonacci numbers
+    the other returns the fibonacci number user requests. (Space complexity is lesser with the latter than former).
 
     Note: Fibonacci is a perfect example of using Dynamic Programming!!
 
@@ -31,17 +30,38 @@ def r_fibonacci(n):
     return r_fibonacci(n-1) + r_fibonacci(n - 2)
 
 
-def dp_fibonacci(n):
+def dp1_fibonacci(n):
+
+    if n <= 1:
+        return n
+
+    prev1 = 0
+    prev2 = 1
+    result = prev1 + prev2
+
+    for idx in range(2, n+1):
+
+        result = prev1 + prev2
+        prev1 = prev2
+        prev2 = result
+
+    return result
+
+
+def dp2_fibonacci(n):
+    """
+    :param n: fibo numb
+    :return: This function is if you want to return the entire array of fibnacci numbers
+    """
 
     dp = [0]*(n+1)
 
-    dp[0] = 0
-    dp[1] = 1
-
+    if n <= 1:
+        return n
     for idx in range(2, n+1):
         dp[idx] = dp[idx - 1] + dp[idx - 2]
 
-    return dp[idx]
+    return dp
 
 
 def main():
@@ -56,7 +76,7 @@ def main():
     print('Recursion Time: ', (end_time - start_time), 'ms\n\n')
 
     start_time = time.time() * 1000.0
-    print('The fibonacci number at', user_input, 'is:', dp_fibonacci(int(user_input)), 'using dp.')
+    print('The fibonacci number at', user_input, 'is:', dp1_fibonacci(int(user_input)), 'using dp.')
     end_time = time.time() * 1000.0
     print('Dynamic Programming Time: ', (end_time - start_time), 'ms')
 
