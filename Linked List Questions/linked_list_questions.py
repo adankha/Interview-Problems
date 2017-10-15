3e4170a99cc0cacc872b28a9def91394251b49e8
Ashour Dankha <dankha90@gmail.com>
2017-10-14 22:50:36 -0500

    inserted in sorted ll

--- Linked List Questions/linked_list_questions.py
+++ Linked List Questions/linked_list_questions.py
@@ -21,6 +21,35 @@ def find_middle(head):
         iter2 = iter2.next.next
     return iter1.data
 
+
+def insert_num(the_list, num):
+
+    head = the_list.head
+
+    if head is None:
+        the_list.add(num)
+        return
+    if num <= head.data:
+        the_list.add(num)
+        return
+
+    cur_node = head
+    next_node = head.next
+    new_node = Node(num)
+
+    while cur_node is not None:
+
+        if next_node is not None and cur_node.data < num <= next_node.data:
+            cur_node.next = new_node
+            new_node.next = next_node
+            return
+        elif next_node is None and cur_node.data <= num:
+            cur_node.next = new_node
+            return
+        cur_node = cur_node.next
+        next_node = next_node.next
+
+
 def random_list():
     my_list = LinkedList()
     my_list.add(31)
@@ -50,7 +79,12 @@ def main():
 
     print('Sorted Linked List: ')
     sorted_list.print_list()
-
+    insert_num(sorted_list, 15)
+    insert_num(sorted_list, -1)
+    insert_num(sorted_list, 22)
+    insert_num(sorted_list, -10)
+    print('Sorted Linked List After Insertions 15, -1, 22, -10: ')
+    sorted_list.print_list()
 
 
 if __name__ == '__main__':
